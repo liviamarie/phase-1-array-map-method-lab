15b8983fd5d7b08d5f6490e4aa1584c5faa37392
olivia <ojeglum@gmail.com>
2021-09-28 14:45:18 -0700

    commit tree 3da4eee505855b1dec5ba12d117adac34975b056

--- index.js
+++ index.js
@@ -11,6 +11,39 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// const titleCased = tutorials.map(function(titles) {
+//   return tutorials.toTitleCase();
+// })
+
+// function titleCased(str) {
+//   return str.toLowerCase().split(' ').map(function(word) {
+//     return word.replace(word[0], word[0].toUpperCase());
+//   }).join(' ');
+// }
+
+// const titleCased = () => {
+//   const cap = tutorials.split(' ');
+//   cap.map(first => first[0].toUpperCase() + first.substring(1)).join(' ');
+//   return tutorials;
+// }
+
+// const titleCased = tutorials => {
+//   tutorials.map(function (element) {
+//     const words = element.split(" ");
+//     words.map((word) => {
+//       return word[0].toUpperCase() + word.substring(1);
+//     }).join(" ");
+//   })
+// }
+
+function capitalize(sentence) {
+  const words = sentence.split(' ');
+  const result = words.map(e => e[0].toUpperCase() + e.slice(1));
+  const newSentence = result.join(' ')
+  return newSentence;
 }
+
+function titleCased() {
+  return tutorials.map(capitalize);
+}
+
